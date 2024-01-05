5b5b343c14c195969f2939c3ff860a692734e4d6
Michael Sjöberg <mail@michaelsjoeberg.com>
2024-01-05 22:22:27 +0800

    Update mutate.py

--- mutate.py
+++ mutate.py
@@ -83,6 +83,87 @@ def generate_population(f, n_pop, x_range, y_range, m_bits):
 
     return pop_lst
 
+def generate_offsprings(population, crossover):
+    """Generate offsprings
+
+    population (list)       : population
+    crossover (list)        : crossover points
+    """
+    n = 0
+    offsprings_lst = []
+    
+    while n < len(population):
+        offsprings_lst.append(
+            population[n][1][0:crossover[0]] +
+            population[n + 1][1][crossover[0]:crossover[1]] +
+            population[n][1][crossover[1]:]
+        )
+        offsprings_lst.append(
+            population[n + 1][1][0:crossover[0]] +
+            population[n][1][crossover[0]:crossover[1]] +
+            population[n + 1][1][crossover[1]:]
+        )
+        # pair-wise
+        n += 2
+
+    return offsprings_lst
+
+def mutate(offsprings, m_bits, mu=0.1):
+    """Mutate offsprings
+
+    offsprings (list)       : offsprings
+    mu (float)              : mutation rate
+    m_bits (int)            : number of bits
+    """
+    nbits = round(mu * (len(offsprings) * m_bits * 2))
+    for i in range(nbits):
+        offspring = random.randint(0, len(offsprings) - 1)
+        bit = random.randint(0, m_bits * 2 - 1)
+        # flip bits
+        if offsprings[offspring][bit] == 1:
+            offsprings[offspring][bit] = 0
+        else:
+            offsprings[offspring][bit] = 1
+
+    return offsprings
+
+def update_population(f, current_population, offsprings, keep, x_range, y_range, m_bits):
+    """Update population
+
+    f (function)                : cost function
+    current_population (list)   : current population
+    offsprings (list)           : offsprings
+    keep (int)                  : number of population to keep
+    x_range (list)              : range of x
+    y_range (list)              : range of y
+    m_bits (int)                : number of bits
+    """
+    offsprings_lst = []
+    for i in range(len(offsprings)):
+        # decoded values
+        x_decoded = round(decode(offsprings[i][:4], x_range[0], x_range[1], m_bits), 2)
+        y_decoded = round(decode(offsprings[i][4:], y_range[0], y_range[1], m_bits), 2)
+        # determine initial cost
+        cost = round(f(x_decoded, y_decoded), 2)
+        # append to list
+        offsprings_lst.append([i, offsprings[i], [x_decoded, y_decoded], cost])
+    # sort on cost
+    offsprings_lst.sort(key = lambda x: x[3])
+    # update index
+    for i in range(len(offsprings_lst)):
+        offsprings_lst[i][0] = i
+    # discard current population
+    current_population[keep:] = offsprings_lst[:keep]
+    # sort on cost
+    current_population.sort(key = lambda x: x[3])
+    # update index
+    for i in range(len(current_population)):
+        current_population[i][0] = i
+
+    return current_population
+
+### ---
+
 def parse_function(line):
     fdef, ffun = line.split("->")[0], line.split("->")[1]
     args = fdef.split(":")[1].strip()
@@ -116,9 +197,10 @@ def parse_population(line):
 
 # f: (x, y) -> -x * (y / 2 - 10); [10, 20] [-5, 7]
 # p: 10 4
+# mutate! 10
 
 if __name__ == "__main__":
-    fun, npop, mbits = None, None, None
+    fun, pop, npop, mbits, offsprings = None, None, None, None, None
     symbols = []
     ranges = []
     
@@ -136,3 +218,30 @@ if __name__ == "__main__":
 
         if line[0] == "p":
             pop, npop, mbits = parse_population(line)
+
+        if line[0] == "m":
+            args = line.split(" ")
+            if len(args) > 1:
+                MAX_RUNS = int(line.split(" ")[1])
+            else:
+                MAX_RUNS = 100
+
+            for i in range(MAX_RUNS):
+                # generate offsprings
+                offsprings = generate_offsprings(pop, [3, 6])
+                # mutate
+                offsprings = mutate(offsprings, mbits)
+                # update population
+                pop = update_population(
+                    fun,
+                    pop,
+                    offsprings,
+                    2,
+                    ranges[0],
+                    ranges[1],
+                    mbits
+                )
+
+            print_as_table(pop)
+
+
